fee476290fd70fad2c71559b8d42b4012a745caa
liujinye <liujinye@xiaomi.com>
2020-04-30 16:58:43 +0800

    Check that there is enough space for AvbDescriptor am: 66872e8dc9 am: b7fe9c3b20 am: 5a2b0ed1b6
    Change-Id: I795c40fecb306b3134f3ce5c4eb1bbc5b60cac63

--- test/avb_aftl_verify_unittest.cc
+++ test/avb_aftl_verify_unittest.cc
@@ -125,32 +125,23 @@ class AvbAftlVerifyTest : public BaseAvbToolTest,
 };
 
 TEST_F(AvbAftlVerifyTest, Basic) {
-  AftlSlotVerifyResult result =
+  AvbSlotVerifyResult result =
       aftl_slot_verify(ops_.avb_ops(), asv_test_data_, key_bytes_, key_size_);
-  EXPECT_EQ(result, AFTL_SLOT_VERIFY_RESULT_OK);
+  EXPECT_EQ(result, AVB_SLOT_VERIFY_RESULT_OK);
 }
 
-TEST_F(AvbAftlVerifyTest, PartitionError) {
+TEST_F(AvbAftlVerifyTest, MissingAFTLDescriptor) {
   asv_test_data_->vbmeta_images[0].partition_name = (char*)"do-no-exist";
-  AftlSlotVerifyResult result =
+  AvbSlotVerifyResult result =
       aftl_slot_verify(ops_.avb_ops(), asv_test_data_, key_bytes_, key_size_);
-  EXPECT_EQ(result, AFTL_SLOT_VERIFY_RESULT_ERROR_IMAGE_NOT_FOUND);
+  EXPECT_EQ(result, AVB_SLOT_VERIFY_RESULT_ERROR_INVALID_METADATA);
 }
 
-TEST_F(AvbAftlVerifyTest, MismatchingVBMeta) {
+TEST_F(AvbAftlVerifyTest, NonMatchingVBMeta) {
   asv_test_data_->vbmeta_images[0].vbmeta_data[0] = 'X';
-  AftlSlotVerifyResult result =
+  AvbSlotVerifyResult result =
       aftl_slot_verify(ops_.avb_ops(), asv_test_data_, key_bytes_, key_size_);
-  EXPECT_EQ(result, AFTL_SLOT_VERIFY_RESULT_ERROR_VBMETA_HASH_MISMATCH);
-}
-
-TEST_F(AvbAftlVerifyTest, InvalidKey) {
-  // Corrupt the key in order to fail the verification: complement the last
-  // byte, we keep the key header valid.
-  key_bytes_[key_size_ - 1] = ~key_bytes_[key_size_ - 1];
-  AftlSlotVerifyResult result =
-      aftl_slot_verify(ops_.avb_ops(), asv_test_data_, key_bytes_, key_size_);
-  EXPECT_EQ(result, AFTL_SLOT_VERIFY_RESULT_ERROR_INVALID_PROOF_SIGNATURE);
+  EXPECT_EQ(result, AVB_SLOT_VERIFY_RESULT_ERROR_VERIFICATION);
 }
 
 } /* namespace avb */
