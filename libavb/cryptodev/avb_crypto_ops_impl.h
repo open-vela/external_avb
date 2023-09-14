3e12dceb1880a2414af4c6e26ad4b70369a893d4
makejian <makejian@xiaomi.com>
2023-09-14 17:56:54 +0800

    avb/sha: add sha accelerator via /dev/crypto
    Change-Id: I36d77e55e54f7458ab81a9fc56d680ffbc7ba89a

---
+++ libavb/cryptodev/avb_crypto_ops_impl.h
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef AVB_CRYPTO_OPS_IMPL_H_
+#define AVB_CRYPTO_OPS_IMPL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#include <crypto/cryptodev.h>
+
+typedef struct avb_cryptodev_context_s {
+  int fd;
+  struct session_op session;
+  struct crypt_op crypt;
+} avb_cryptodev_context_t;
+
+#define AVB_SHA256_CONTEXT_SIZE sizeof(avb_cryptodev_context_t)
+#define AVB_SHA512_CONTEXT_SIZE sizeof(avb_cryptodev_context_t)
+
+static inline int avb_cryptodev_init(avb_cryptodev_context_t *ctx) {
+  int ret;
+  int fd;
+
+  memset(ctx, 0, sizeof(avb_cryptodev_context_t));
+  fd = open("/dev/crypto", O_RDWR, 0);
+  if (fd < 0) {
+    return -errno;
+  }
+
+  ret = ioctl(fd, CRIOGET, &ctx->fd);
+  close(fd);
+  if (ret < 0) {
+    ret = -errno;
+  }
+
+  return ret;
+}
+
+static inline void avb_cryptodev_free(avb_cryptodev_context_t *ctx) {
+  close(ctx->fd);
+  memset(ctx, 0, sizeof(avb_cryptodev_context_t));
+}
+
+static inline int avb_cryptodev_get_session(avb_cryptodev_context_t *ctx) {
+  int ret;
+
+  ret = ioctl(ctx->fd, CIOCGSESSION, &ctx->session);
+  if (ret < 0) {
+    return -errno;
+  }
+
+  ctx->crypt.ses = ctx->session.ses;
+  return ret;
+}
+
+static inline void avb_cryptodev_free_session(avb_cryptodev_context_t *ctx) {
+  ioctl(ctx->fd, CIOCFSESSION, &ctx->session.ses);
+  ctx->crypt.ses = 0;
+}
+
+static inline int avb_cryptodev_crypt(avb_cryptodev_context_t *ctx) {
+  int ret;
+
+  ret = ioctl(ctx->fd, CIOCCRYPT, &ctx->crypt);
+  return ret < 0 ? -errno : ret;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AVB_CRYPTO_OPS_IMPL_H_ */
---
+++ libavb/cryptodev/sha256_cryptodev.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <libavb/avb_sha.h>
+#include <libavb/avb_util.h>
+
+void avb_sha256_init(AvbSHA256Ctx* avb_ctx) {
+    avb_cryptodev_context_t* ctx = (avb_cryptodev_context_t*)avb_ctx->reserved;
+    avb_assert(avb_cryptodev_init(ctx) == 0);
+    ctx->session.mac = CRYPTO_SHA2_256;
+    avb_assert(avb_cryptodev_get_session(ctx) == 0);
+}
+
+void avb_sha256_update(AvbSHA256Ctx* avb_ctx, const uint8_t* data, size_t len) {
+    avb_cryptodev_context_t* ctx = (avb_cryptodev_context_t*)avb_ctx->reserved;
+    ctx->crypt.op = COP_ENCRYPT;
+    ctx->crypt.flags |= COP_FLAG_UPDATE;
+    ctx->crypt.src = (caddr_t)data;
+    ctx->crypt.len = len;
+    avb_assert(avb_cryptodev_crypt(ctx) == 0);
+}
+
+uint8_t* avb_sha256_final(AvbSHA256Ctx* avb_ctx) {
+    avb_cryptodev_context_t* ctx = (avb_cryptodev_context_t*)avb_ctx->reserved;
+    ctx->crypt.op = COP_ENCRYPT;
+    ctx->crypt.flags = 0;
+    ctx->crypt.mac = (caddr_t)avb_ctx->buf;
+    avb_assert(avb_cryptodev_crypt(ctx) == 0);
+    avb_cryptodev_free_session(ctx);
+    avb_cryptodev_free(ctx);
+    return avb_ctx->buf;
+}
