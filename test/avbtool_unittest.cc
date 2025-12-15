jbeich/platform_external_avb

avbtool: Add "print_partition_digests" sub-command.
This goes through descriptors in the given vbmeta image as well and
chained images and prints out the digests for hash- and hashtree
descriptors.

This works with both python2 and python3.

Also add a paragraph documenting the "info_image" sub-command since
that was missing.

Test: New unit test and all unit tests pass.
Bug: 152063016
Change-Id: I0525d443686f42e8de7e02a3dc1ea97643d2fec1

test/avbtool_unittest.cc
@@ -2905,6 +2905,88 @@ TEST_F(AvbToolTest, VerifyImageChainPartitionOtherVBMeta) {
                  vbmeta_google_path.value().c_str());
 }
 
+TEST_F(AvbToolTest, PrintPartitionDigests) {
+  base::FilePath pk4096_path = testdir_.Append("testkey_rsa4096.avbpubkey");
+  EXPECT_COMMAND(
+      0,
+      "./avbtool extract_public_key --key test/data/testkey_rsa4096.pem"
+      " --output %s",
+      pk4096_path.value().c_str());
+
+  const size_t boot_partition_size = 16 * 1024 * 1024;
+  const size_t boot_image_size = 5 * 1024 * 1024;
+  base::FilePath boot_path = GenerateImage("boot.img", boot_image_size);
+  EXPECT_COMMAND(0,
+                 "./avbtool add_hash_footer"
+                 " --image %s"
+                 " --rollback_index 0"
+                 " --partition_name boot"
+                 " --partition_size %zd"
+                 " --salt deadbeef"
+                 " --internal_release_string \"\"",
+                 boot_path.value().c_str(),
+                 boot_partition_size);
+
+  GenerateVBMetaImage("vbmeta.img",
+                      "SHA256_RSA2048",
+                      0,
+                      base::FilePath("test/data/testkey_rsa2048.pem"),
+                      base::StringPrintf("--chain_partition system:1:%s "
+                                         "--include_descriptors_from_image %s",
+                                         pk4096_path.value().c_str(),
+                                         boot_path.value().c_str()));
+
+  const size_t system_partition_size = 10 * 1024 * 1024;
+  const size_t system_image_size = 8 * 1024 * 1024;
+  base::FilePath system_path = GenerateImage("system.img", system_image_size);
+  EXPECT_COMMAND(0,
+                 "./avbtool add_hashtree_footer --salt d00df00d --image %s "
+                 "--partition_size %zd --partition_name system "
+                 "--algorithm SHA256_RSA4096 "
+                 "--key test/data/testkey_rsa4096.pem "
+                 "--internal_release_string \"\" ",
+                 system_path.value().c_str(),
+                 system_partition_size);
+
+  base::FilePath out_path = testdir_.Append("out.txt");
+  std::string out;
+
+  // Normal output
+  EXPECT_COMMAND(0,
+                 "./avbtool print_partition_digests --image %s --output %s",
+                 vbmeta_image_path_.value().c_str(),
+                 out_path.value().c_str());
+  ASSERT_TRUE(base::ReadFileToString(out_path, &out));
+  EXPECT_EQ(
+      "system: d52d93c988d336a79abe1c05240ae9a79a9b7d61\n"
+      "boot: "
+      "184cb36243adb8b87d2d8c4802de32125fe294ec46753d732144ee65df68a23d\n",
+      out);
+
+  // JSON output
+  EXPECT_COMMAND(
+      0,
+      "./avbtool print_partition_digests --image %s --json --output %s",
+      vbmeta_image_path_.value().c_str(),
+      out_path.value().c_str());
+  ASSERT_TRUE(base::ReadFileToString(out_path, &out));
+  EXPECT_EQ(
+      "{\n"
+      "  \"partitions\": [\n"
+      "    {\n"
+      "      \"name\": \"system\",\n"
+      "      \"digest\": \"d52d93c988d336a79abe1c05240ae9a79a9b7d61\"\n"
+      "    },\n"
+      "    {\n"
+      "      \"name\": \"boot\",\n"
+      "      \"digest\": "
+      "\"184cb36243adb8b87d2d8c4802de32125fe294ec46753d732144ee65df68a23d\"\n"
+      "    }\n"
+      "  ]\n"
+      "}\n",
+      out);
+}
+
 class AvbToolTest_PrintRequiredVersion : public AvbToolTest {
  protected:
   const char* kOutputFile = "versions.txt";

open-vela/external_avb

test/avbtool_unittest.cc
@@ -2905,6 +2905,90 @@ TEST_F(AvbToolTest, VerifyImageChainPartitionOtherVBMeta) {
                  vbmeta_google_path.value().c_str());
 }
 
+TEST_F(AvbToolTest, PrintPartitionDigests) {
+  base::FilePath pk4096_path = testdir_.Append("testkey_rsa4096.avbpubkey");
+  EXPECT_COMMAND(
+      0,
+      "./avbtool extract_public_key --key test/data/testkey_rsa4096.pem"
+      " --output %s",
+      pk4096_path.value().c_str());
+
+  const size_t boot_partition_size = 16 * 1024 * 1024;
+  const size_t boot_image_size = 5 * 1024 * 1024;
+  base::FilePath boot_path = GenerateImage("boot.img", boot_image_size);
+  EXPECT_COMMAND(0,
+                 "./avbtool add_hash_footer"
+                 " --image %s"
+                 " --rollback_index 0"
+                 " --partition_name boot"
+                 " --partition_size %zd"
+                 " --salt deadbeef"
+                 " --internal_release_string \"\"",
+                 boot_path.value().c_str(),
+                 boot_partition_size);
+
+  GenerateVBMetaImage("vbmeta.img",
+                      "SHA256_RSA2048",
+                      0,
+                      base::FilePath("test/data/testkey_rsa2048.pem"),
+                      base::StringPrintf("--chain_partition system:1:%s "
+                                         "--include_descriptors_from_image %s",
+                                         pk4096_path.value().c_str(),
+                                         boot_path.value().c_str()));
+
+  const size_t system_partition_size = 10 * 1024 * 1024;
+  const size_t system_image_size = 8 * 1024 * 1024;
+  base::FilePath system_path = GenerateImage("system.img", system_image_size);
+  EXPECT_COMMAND(0,
+                 "./avbtool add_hashtree_footer --salt d00df00d --image %s "
+                 "--partition_size %zd --partition_name system "
+                 "--algorithm SHA256_RSA4096 "
+                 "--key test/data/testkey_rsa4096.pem "
+                 "--internal_release_string \"\" ",
+                 system_path.value().c_str(),
+                 system_partition_size);
+
+  base::FilePath out_path = testdir_.Append("out.txt");
+  std::string out;
+
+  // Normal output
+  EXPECT_COMMAND(0,
+                 "./avbtool print_partition_digests --image %s --output %s",
+                 vbmeta_image_path_.value().c_str(),
+                 out_path.value().c_str());
+  ASSERT_TRUE(base::ReadFileToString(out_path, &out));
+  EXPECT_EQ(
+      "system: d52d93c988d336a79abe1c05240ae9a79a9b7d61\n"
+      "boot: "
+      "184cb36243adb8b87d2d8c4802de32125fe294ec46753d732144ee65df68a23d\n",
+      out);
+
+  // JSON output
+  EXPECT_COMMAND(
+      0,
+      "./avbtool print_partition_digests --image %s --json --output %s",
+      vbmeta_image_path_.value().c_str(),
+      out_path.value().c_str());
+  ASSERT_TRUE(base::ReadFileToString(out_path, &out));
+  // The trailing whitespace comes from python. If they fix that bug we need
+  // to update this test...
+  EXPECT_EQ(
+      "{\n"
+      "  \"partitions\": [\n"
+      "    {\n"
+      "      \"name\": \"system\", \n"
+      "      \"digest\": \"d52d93c988d336a79abe1c05240ae9a79a9b7d61\"\n"
+      "    }, \n"
+      "    {\n"
+      "      \"name\": \"boot\", \n"
+      "      \"digest\": "
+      "\"184cb36243adb8b87d2d8c4802de32125fe294ec46753d732144ee65df68a23d\"\n"
+      "    }\n"
+      "  ]\n"
+      "}",
+      out);
+}
+
 class AvbToolTest_PrintRequiredVersion : public AvbToolTest {
  protected:
   const char* kOutputFile = "versions.txt";